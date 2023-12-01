475d54c77cb11e8411cfd5f4106a5402c4de9684
SimonBalling <simon.balling@ffuf.de>
2023-12-01 16:14:34 +0100

    Day 1

---
+++ 01/NumbersMap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    public static class NumbersMap
+    {
+        public static Dictionary<string, string> Map = new Dictionary<string, string>()
+        {
+            { "one", "1" },
+            { "1", "1" },
+            { "two", "2" },
+            { "2", "2" },
+            { "three", "3" },
+            { "3", "3" },
+            { "four", "4" },
+            { "4", "4" },
+            { "five", "5" },
+            { "5", "5" },
+            { "six", "6" },
+            { "6", "6" },
+            { "seven", "7" },
+            { "7", "7" },
+            { "eight", "8" },
+            { "8", "8" },
+            { "nine", "9" },
+            { "9", "9" }
+        };
+    }
+}
---
+++ 01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using _01;
+
+(string first, string last) GetDigits(Regex regex, string s)
+{
+    var matches = regex.Matches(s);
+    var first1 = matches.First().Groups[1].Value;
+    var last1 = matches.Last().Groups[1].Value;
+    return (first1, last1);
+}
+
+// Part 1
+var regexPart1 = new Regex("([0-9])");
+
+// Part 2
+var regexPart2 = new Regex("(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))");
+
+using var sr = new StreamReader(File.OpenRead("Data\\input.txt"));
+var sum = 0;
+var line = sr.ReadLine();
+
+while (line is not null)
+{
+    var (first, last) = GetDigits(regexPart1, line);
+
+    var code = $"{NumbersMap.Map[first]}{NumbersMap.Map[last]}";
+
+    Console.WriteLine(code);
+
+    sum += Convert.ToInt32(code); 
+    line = sr.ReadLine();
+}
+
+Console.WriteLine($"Sum is {sum}");
